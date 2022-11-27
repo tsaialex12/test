ed6c40fb8168deb0d5dd71b616b489934c8d1386
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-11-27 22:30:22 +0800

    Create test02.java

---
+++ test02.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class data{
+    private String name;
+    private  Test score;
+    class Test{
+        private int eng;
+        private int m;
+    }
+    void name(String a,String b){
+
+    }
+    double avg() {
+
+    }
+
+
+    }
+public class Class02 {
+    public static void main(String[] args) {
+        data nn=new data();
+        nn.name("Annie","Brain");
+    }
+}
